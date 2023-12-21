ae8b878473b8cfd7046ae7d58ec23647c0c5ab11
Aloysio de Medeiros Winter <81388032+aloysiowinter@users.noreply.github.com>
2023-12-21 08:41:38 -0300

    feedback day one justinkroegerlake

--- Advent_of_Code/2023/Dia01/solution_day_1.py
+++ Advent_of_Code/2023/Dia01/solution_day_1.py
@@ -7,6 +7,8 @@ Description: Solution of day one of Advent Of Code 2023.
 
 import re
 
+word_to_number = {"one": 1 , "two" : 2 , "three" : 3 , "four" : 4 , "five" : 5 , "six" : 6 , "seven" : 7 , "eight" : 8 , "nine" : 9}
+
 def transform_to_number(num):
     """
     Converts a numeric or string representation of a number into an integer.
@@ -17,11 +19,10 @@ def transform_to_number(num):
     Returns:
     int: The integer representation of the input number.
     """
-    dict = {"one": 1 , "two" : 2 , "three" : 3 , "four" : 4 , "five" : 5 , "six" : 6 , "seven" : 7 , "eight" : 8 , "nine" : 9}
-    if num.isnumeric():
-        return int(num)
-    else:
-        return dict[num]
+    global word_to_number
+
+    return int(num) if num.isnumeric() else word_to_number[num]
+   
 
 def get_solution(pattern,input_file_name):
     """
@@ -36,7 +37,7 @@ def get_solution(pattern,input_file_name):
     """
 
     # Open the input file
-    with open(input_file_name,'r') as input_file:
+    with open(input_file_name) as input_file:
         
         calibration_value_list = [] # List to store the calibration value of each line
 
@@ -44,9 +45,8 @@ def get_solution(pattern,input_file_name):
         for line in input_file:  
             line_digits = [digit.group() for digit in pattern.finditer(line)]#list of each digit in the line
 
-            # Concatenate the first digit and the last digit to form a single two-digit number
-            concatenated_number = str(transform_to_number(line_digits[0])) + str(transform_to_number(line_digits[-1]))
-            calibration_value_list.append(int(concatenated_number)) #append the calibration value in the list
+            # Combining the first digit and the last digit to form a single two-digit number
+            calibration_value_list.append((transform_to_number(line_digits[0])*10)+transform_to_number(line_digits[-1]))
 
         #return the sum of each calibration value
         return sum(calibration_value_list)
@@ -57,8 +57,7 @@ pattern_part_2 = re.compile(r'(one|two|three|four|five|six|seven|eight|nine|\d)'
 
 input_file_name = 'input.txt'
 
-print("part one: " + str(get_solution(pattern_part_1,input_file_name)))   
-print("part two: " + str(get_solution(pattern_part_2,input_file_name)))
-    
+print(f"part one: {get_solution(pattern_part_1,input_file_name)}")
+print(f"part two: {get_solution(pattern_part_2,input_file_name)}")
 
     
